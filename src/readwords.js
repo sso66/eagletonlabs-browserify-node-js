d6ea7a2aa7bae6a7419c5396d1d3a83dab9ea9fd
sso66 <sun.stephen.oo@gmail.com>
2019-09-30 20:41:01 -0400

    clean up repo

--- src/readwords.js
+++ src/readwords.js
@@ -9,7 +9,10 @@ console.log(censor.getCensoredWords());
 console.log(censor.censor("\nA very gloomy up day.\n"));
 console.log("-----------------------------------------------");
 
+// render node app in the browser
 const content = document.querySelector('#content');
+
 content.innerHTML = '<i>'+censor.getCensoredWords()+'</i><br />';
-content.innerHTML += censor.censor("Some very sad, bad and mad text.");
+content.innerHTML += censor.censor("Some very sad, bad and mad text. <hr/>");
+content.innerHTML += "Render Node App in the Browser";
 // eof 
