1b18a9873c1751241864c7fa5c32b39d78615503
sso66 <sun.stephen.oo@gmail.com>
2020-03-07 16:35:02 -0500

    complete plan for teardown

--- src/os_util_dns/dns_lookup.js
+++ src/os_util_dns/dns_lookup.js
@@ -1,20 +1,19 @@
 // File: dns_lookup.js
-// Note:
-// Date:
+// Note: Perform lookups and reverse lookups on domains and IP addresses
+// Date: 03/07/2020
 //.............................................................................. 
 console.info('Mounting dns_lookup.js...\n');
 
 var dns = require('dns');
 
-console.log('Retrieving www.cnn.com...');
-
-dns.resolve('www.cnn.com', function(err, addresses) {
-  console.log('IPV6 addresses' + JSON.stringify(addresses, false, ' '));
-  addresses.forEach(function(addr) {
-    dns.reverse(addr, function(err, domains) {
-      console.log('Reverse for ' + addr + ': ' + JSON.stringify(domains));
+console.log('Retrieving www.google.com...');
+dns.resolve4('www.google.com', function(err, addresses) {
+    console.log('IPv4 addresses' + JSON.stringify(addresses, false, ' '));
+    addresses.forEach(function(addr) {
+        dns.reverse(addr, function(err, domains) {
+            console.log('Reverse lookup for ' + addr + ': ' + JSON.stringify(domains));
+        });
     });
-  });
 });
 
 // eof
