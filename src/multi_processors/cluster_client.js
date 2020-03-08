cdc59f7f136328150925b657bd044ba3eb57e560
sso66 <sun.stephen.oo@gmail.com>
2020-03-08 08:59:23 -0400

    add websockets, tcp and tls sockets

--- src/multi_processors/cluster_client.js
+++ src/multi_processors/cluster_client.js
@@ -1,4 +1,24 @@
 // File: cluster_client.js
-// Note:
-// Date:
+// Note: An HTTP client sending a series of requests to test the server
+// Date: 03/07/2020
 //..............................................................................
+console.log( "Mounting cluster_client.js..." );
+
+var http = require('http');
+var options = { port: '8080' };
+function sendRequest() {
+    http.request(options, function(response) {
+        response.on('data', function() {
+            serverData += chunk;
+        });
+        response.on('end', function() {
+            console.log(serverData);
+        });
+    }).end();
+}
+for (var i=0; i=5; i++) {
+    console.log("Sending Request");
+    sendRequest();
+}
+
+// eof
--- src/multi_processors/cluster_worker.js
+++ src/multi_processors/cluster_worker.js
@@ -1,4 +1,19 @@
 // File: cluster_info.js
-// Note:
-// Date:
+// Note: A worker process implementing an HTTP server
+// Date: 03/07/2020
 //..............................................................................
+console.log( "Mounting cluster_server.js..." );
+
+var cluster = require('cluster');
+var http = require('http');
+if (cluster.isWorker) {
+    http.Server(function(req, res) {
+       res.writeHead(200);
+       res.end("Process " + process.id + "says hello");
+       process.send("Process " + process.pid + " handled request"); 
+    }).listen(8080, function() {
+        console.log("Child Server Running on Process " + process.id);
+    });
+}
+
+// eof
