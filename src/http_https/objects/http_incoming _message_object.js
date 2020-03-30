b8a6818c0d4cb5af78eb9954915385a1fe71ea6b
sso66 <sun.stephen.oo@gmail.com>
2020-03-30 11:41:04 -0400

    update HTTP/HTTPS module usecases

--- src/http_https/objects/http_incoming _message_object.js
+++ src/http_https/objects/http_incoming _message_object.js
@@ -1,8 +1,44 @@
 // File: http_incoming_message_object.js
 // Note: Implementing http.IncomingMessage object
-// Date: 3/28/2020
+// Date: 3/30/2020
 //..............................................................................
 console.log("Mounting http_incoming_message_object.js...\n");
-
+/*
+ * Either the HTTP server or the HTTP client creates the http.IncomingMessage
+ * object.
+ * 
+ * On the server side the client request is represented by an 
+ * http.IncomingMessage object, on the clientside the server response is
+ * represented by an http.IncomingMessage object. 
+ * 
+ * The reason that the http.IncomingMessage object can be used for both is that
+ * the functionality is basically the same.
+ * 
+ * The http.IncomingMessage object implements a << Readable >> stream, allowing
+ * you to read the client request or server response as a streaming source.
+ * 
+ * In addition to the functionality provided by the << Readable >> class,
+ * http.IncomingMessage objects als provide the properties, events and 
+ * methods. 
+ * 
+ * These following list allows you to access informaiton from the client request
+ * or the server response.
+ * 
+ * Properties:
+ * - httpVersion
+ * - headers
+ * - trailers
+ * - method
+ * - url
+ * - statusCode
+ * - socket
+ * 
+ * Events:
+ * close
+ * 
+ * Methods:
+ * - setTimeout(msecs, callback)
+ * 
+ */
 
 // eof
--- src/http_https/objects/http_server_response_object.js
+++ src/http_https/objects/http_server_response_object.js
@@ -1,8 +1,45 @@
 // File: http_server_response_object.js
 // Note: Implementing http.ServerResponse object
-// Date: 3/28/2020
+// Date: 3/30/2020
 //..............................................................................
 console.log("Mounting http_server_response_object.js...\n");
-
+/*
+ * The HTTP server create the http.ServerResponse object internally when it 
+ * receives a request event.
+ * 
+ * This object is passed to the request event handler as the second argument.
+ * 
+ * You use the http.ServerResponse object to formulate and send a response to 
+ * the client.
+ * 
+ * The http.ServerResponse object implements a << Writable >> stream, so it
+ * provides all the functionaly of a << Writable >> object. For example, you 
+ * can use the write() method to write to the http.ServerResponse object as 
+ * well as pipe a << Readable >> stream into it to write data back to the 
+ * client
+ * 
+ * When handling the client request, you use the properties, events and methods
+ * of the http.ServerObject to build and send headers; write data and send the 
+ * response:
+ * 
+ * Properties:
+ * - headersSent
+ * - sendDate
+ * - statusCode
+ * 
+ * Events:
+ * - close
+ * 
+ * Methods:
+ * - writeContinue()
+ * - writeHead(statusCode, [reasonPhrase], [headers])
+ * - setTimeout((msecs, callback)
+ * - setHeader(name, value)
+ * - removeHeader(name)
+ * - write(chunk, [enchoding])
+ * - addTrailers(headers)
+ * - end([data], [encoding])
+ * 
+ */
 
 // eof
