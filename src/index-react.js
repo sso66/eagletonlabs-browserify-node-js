d5fecc11c10a1ed51109f486bdc04b556d274bbf
sso66 <sun.stephen.oo@gmail.com>
2020-03-31 15:58:55 -0400

    finalize node.js modules

--- src/index-react.js
+++ src/index-react.js
@@ -3,9 +3,6 @@ console.log('Mounting index-react.js Node.js Application...');
 
 var ReactDOM = require('react-dom');
 var React = require('react');
- 
-// var Chat = require('./Chat');
-// var Contacts = require('./Contacts');
 
 const square = require('./square');
 console.log("Browserify: " + square(125)); //  15625
