b5f07d1e841e4d6417d32094cfa311ac4c473b8f
sso66 <sun.stephen.oo@gmail.com>
2020-04-08 15:58:58 -0400

    add UDP use cases

--- src/datagram/udp_client_ipg.js
+++ src/datagram/udp_client_ipg.js
@@ -9,17 +9,16 @@ var dgram = require('dgram');
 var PORT = 8089;
 var HOST = '127.0.0.1';
 
+// init
 var client = dgram.createSocket("udp4");
 var data = "";
 
 // set command line input character encoding to utf-8
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', function (text) {
-    
-    // if user confim with 'send\r\n' then send all input data to UDP server
-    if('send\r\n' === text) {
-        
+process.stdin.on('data', function (text) {  
+    // if user acknowledged with 'send\r\n' then send all input data to UDP server
+    if('send\r\n' === text) {       
         // if user do not input data in command line then send default data
         if (data == null || data.length == 0) {
             data = "Hello UDP server...";
@@ -28,11 +27,10 @@ process.stdin.on('data', function (text) {
 
         // create a Buffer object to wrap data String object
         message = Buffer.from(data);
-
         // send message to UDP server through UDP client socket
         client.send(message, 0, message.length, PORT, HOST);
     } else {
-        // concat all user input text in message.
+        // concat all user input text data.
         data += text;
     }
  });
