6578024991c63c5c824b598946ff47970af2d45c
Sun 'Stephen' Oo <sun.stephen.oo@gmail.com>
2020-04-08 17:42:03 -0400

    Update udp_multicast.js

--- src/datagram/udp_multicast.js
+++ src/datagram/udp_multicast.js
@@ -7,11 +7,11 @@ console.info('Mounting UDP socket udp.js...');
  * UDP is considered part of the transport layer alongside TCP. Unlike TCP, 
  * UDP is a unreliable and connectionless protocol that does not guarantee 
  * delivery or order of messages. The protocol is, however, much simpler and 
- * faster and supports broadcast and multicast. Because of these properties, UDP 
- * should be an excellent protocol to use for any system that must emit its 
- * state to multiple subscribers. Examples of these systems are online game 
- * engines that must share state between clients, distributed systems where 
- * components must be discoverable, and a blockchain network.
+ * faster and supports broadcast and multicast. Because of these properties, 
+ * UDP should be an excellent protocol to use for any system that must emit 
+ * its state to multiple subscribers. Examples of these systems are online 
+ * game engines that must share state between clients, distributed systems 
+ * where components must be discoverable, and a blockchain network.
  * 
  */
 const PORT = 20000;
@@ -19,8 +19,8 @@ const MULTICAST_ADDR = "233.255.255.255";   // HOST
 // how to connect to and send messages to a UDP multicast group.
 // Here, choose the port that we will bind the socket to and the multicast 
 // address we will use. The multicast group address must be within the 
-// appropriate multicast address space. Much of the space is reserved, but you 
-// can still find some unassigned ad-hoc space such as 
+// appropriate multicast address space. Much of the space is reserved, but 
+// you can still find some unassigned ad-hoc space such as 
 // 233.252.18.0-233.255.255.255.
 
 const dgram = require("dgram");
@@ -30,9 +30,13 @@ const socket = dgram.createSocket({ type: "udp4", reuseAddr: true });
 
 socket.bind(PORT);
 
-// The socket is an instance of EventEmitter, so we can listen to a couple events:
-// The ‘listening’ event is the first event that is fired after binding to 
-// the port. 
+// The socket is an instance of EventEmitter, so we can listen to a couple 
+// events. The ‘listening’ event is the first event that is fired after 
+// binding to the port. 
+//
+// We catch this event and add ourselves to the multicast group. Then, we call 
+// sendMessage every 2.5 seconds which sends a message to the multicast group.
+
 socket.on("listening", function() {
     socket.addMembership(MULTICAST_ADDR);
     setInterval(sendMessage, 2500);
@@ -44,8 +48,6 @@ socket.on("listening", function() {
     );
 });
 
-// We catch this event and add ourselves to the multicast group. Then, we call 
-// sendMessage every 2.5 seconds which sends a message to the multicast group.
 function sendMessage() {
     const message = Buffer.from(`Message from process ${process.pid}`);
     socket.send(message, 0, message.length, PORT, MULTICAST_ADDR, function() {
@@ -54,8 +56,8 @@ function sendMessage() {
 }
 
 // Finally, we listen for the message event and print any messages to the console
-socket.on("message", function(message, rinfo) {
-  console.info(`Message from: ${rinfo.address}:${rinfo.port} - ${message}`);
+socket.on("message", function(message, address) {
+  console.info(`Message from: ${address.address}:${address.port} - ${message}`);
 });
 
 // eof
