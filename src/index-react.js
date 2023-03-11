cecd37b6a8c4a8ad8667f2df65cc33c911b0aaa9
Stephen <sun.stephen.oo@gmail.com>
2023-03-11 15:32:31 -0500

    restart node.js programming

--- src/index-react.js
+++ src/index-react.js
@@ -1,5 +1,5 @@
 // index-react.js
-console.log('Mounting index-react.js Node.js Application...');
+console.log('Mounting index-react.js React App on Node.js...');
 
 var ReactDOM = require('react-dom');
 var React = require('react');
@@ -8,7 +8,9 @@ const square = require('./square');
 console.log("Browserify: " + square(125)); //  15625
 
 ReactDOM.render(
-  <div style={{fontSize: '1.5em', color: '#00F'}}>React JS + Browserify:  {square(125)}</div>,
+  <div style={{ background: 'purple', fontFamily: 'monospace' }}>
+    React JS + Browserify: {square(125)}
+  </div>,
   document.getElementById('app')
 );
 
